0eb5d31ba65707fdc2b48b9df393255a1fcc90d8
deljod22 <deljod22@htl-kaindorf.at>
2025-05-28 21:47:22 +0200

    Added model.ts and movies.json files

---
+++ src/common/model.ts
@@ -0,0 +1,9 @@
+export type Movie = {
+    movie_title: string,
+    genre: string,
+    rating: number,
+    director: string,
+    actor_1: string,
+    actor_2 : string,
+    release_date: Date
+}
